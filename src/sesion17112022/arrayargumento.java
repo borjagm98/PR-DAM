3b74a1e6ea9cc65587911f0b6b560a6ae8bf0b82
borjagm98 <borja.gavilanm@iesfm.onmicrosof.com>
2022-11-18 14:02:35 +0100

    .

--- src/sesion17112022/arrayargumento.java
+++ src/sesion17112022/arrayargumento.java
@@ -3,17 +3,17 @@ package sesion17112022;
 import java.util.Arrays;
 
 public class arrayargumento {
-    int [] arrayorigen = {1, 2, 3, 4, 5};
-    int longitud = calcular_longitud(arrayorigen);
-    imprimirArray(arrayorigen, longitud);
-}
-
+    public static void main(String[] args) {
+        int [] arrayorigen = {1, 2, 3, 4, 5};
+        int longitud = calcular_longitud(arrayorigen);
+        imprimirArray(arrayorigen, longitud);
+    }
 private static void imprimirArray(int[] arrayorigen, int longitud){
     System.out.println("El array: " + Arrays.toString(arrayorigen)+" tiene una longiud de: "+ longitud);
 }
-
 private static int calcular_longitud(int [] arrayorigen){
     int resultado= -1;
     resultado = arrayorigen.length;
     return resultado;
+}
 }
